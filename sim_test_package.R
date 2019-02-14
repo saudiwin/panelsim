8a7b1faa5d19503069bdf044b5cb481598b33a6d
bob_kubinec <rmk7xy@virginia.edu>
2019-02-14 13:21:57 -0500

    package that mostly works

--- sim_test_package.R
+++ sim_test_package.R
@@ -3,4 +3,8 @@ library(twowaysim)
 set.seed(22902)
 
 ## Changing the mean of the within-time slopes
-sim1 <- tw_sim(iter=30, parallel=T, arg="case.eff.mean", at=-2:5,cores = 3)
+sim1 <- tw_sim(iter=10, parallel=T, arg=c('omm.x.case'),at=-2:2,cores = 3)
+
+## plot simulation
+
+tw_plot(sim1)
